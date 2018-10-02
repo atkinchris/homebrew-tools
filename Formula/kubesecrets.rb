a1e4ed18100a69f88e337d2ae827579505066fa2
Chris Atkin <atkinchris@gmail.com>
2018-10-02 14:42:41 +0100

    Kubesecrets -> v0.2.1

--- Formula/kubesecrets.rb
+++ Formula/kubesecrets.rb
@@ -1,9 +1,9 @@
 class Kubesecrets < Formula
   desc "Tool to manage secrets in Kubernetes with kubectl"
   homepage "https://github.com/atkinchris/kubesecrets"
-  url "https://github.com/atkinchris/kubesecrets/releases/download/v0.2.0/kubesecrets-0.2.0.tar.gz"
-  sha256 "a2ff33b66f8e0353afedf955ef5c96e649f990d3062cdfaadef8f76e4b59f5c1"
-  version "0.2.0"
+  url "https://github.com/atkinchris/kubesecrets/releases/download/v0.2.1/kubesecrets-0.2.1.tar.gz"
+  sha256 "7ec6369c17fcec2be5de5d298525523a076456938c8309d5a638dae6be8c1c53"
+  version "0.2.1"
 
   bottle :unneeded
 
