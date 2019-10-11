51db1933c01e9c60acefa5d70d9be6074e5ae876
Chris Atkin <atkinchris@gmail.com>
2019-10-11 13:31:32 +0100

    Update borked formula

--- Formula/statslogger.rb
+++ Formula/statslogger.rb
@@ -2,9 +2,8 @@ class StatsLogger < Formula
   desc "Tool to log system stats to stdout or a file"
   homepage "https://github.com/atkinchris/statslogger"
   url "https://github.com/atkinchris/statslogger/releases/download/v0.1.0/statslogger-0.1.0.tar.gz"
-  sha256 "929e04ebb092c5001b61a2fffa72d78537b9bfd95c5a8e85524163a26b638c4e"
+  sha256 "4277b1d8c65b351256486db3bde3895dc605e5f50fccbaec17ca376b13e45f1c"
   bottle :unneeded
-  depends_on :unneeded
   def install
     bin.install "statslogger"
   end
