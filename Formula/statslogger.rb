b56096517dadd359172f60adfb52908730db071a
Chris Atkin <atkinchris@gmail.com>
2019-10-11 13:45:22 +0100

    Correct formula class

--- Formula/statslogger.rb
+++ Formula/statslogger.rb
@@ -1,8 +1,8 @@
-class StatsLogger < Formula
+class Statslogger < Formula
   desc "Tool to log system stats to stdout or a file"
   homepage "https://github.com/atkinchris/statslogger"
   url "https://github.com/atkinchris/statslogger/releases/download/v0.1.0/statslogger-0.1.0.tar.gz"
-  sha256 "4277b1d8c65b351256486db3bde3895dc605e5f50fccbaec17ca376b13e45f1c"
+  sha256 "6bf543be6bcb9b795af4d1167410cdb9e6ab8c0df7883dc4fbaf8be45c637d29"
   bottle :unneeded
   def install
     bin.install "statslogger"
