6ef7d7fc5c8d24b0b0a262b7ca1deec8bae08e5b
Chris Atkin <atkinchris@gmail.com>
2020-04-09 13:13:02 +0100

    Update statslogger v0.3.0

--- Formula/statslogger.rb
+++ Formula/statslogger.rb
@@ -1,12 +1,45 @@
 class Statslogger < Formula
   desc "Tool to log system stats to stdout or a file"
   homepage "https://github.com/atkinchris/statslogger"
-  url "https://github.com/atkinchris/statslogger/releases/download/v0.2.0/statslogger-0.2.0.tar.gz"
-  sha256 "393e0938171373d787d1345e99ad524a2b49151a6d84040af4f54341137c81b7"
+  url "https://github.com/atkinchris/statslogger/releases/download/v0.3.0/statslogger-0.3.0.tar.gz"
+  sha256 "ee1e57c3a998dd04d9bbf5c6422630f0ab5e6894d33344567473f98343316651"
   bottle :unneeded
+
+  plist_options :startup => true
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+        <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/statslogger</string>
+        <string>--time</string>
+        <string>15</string>
+        <string>--output</string>
+        <string>statslogger.ndjson</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true />
+      <key>KeepAlive</key>
+      <false />
+      <key>WorkingDirectory</key>
+      <string>#{HOMEBREW_PREFIX}/.statslogger/</string>
+      <key>StandardOutPath</key>
+      <string>/dev/null</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/statslogger_err.log</string>
+    </dict>
+    </plist>
+    EOS
+  end
+
   def install
     bin.install "statslogger"
   end
+
   test do
     system "#{bin}/statslogger", "--version"
   end
